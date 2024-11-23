43bfe23396fd39cf16e5712861511cea0e94c87c
Dhruv Chokshi <168044980+chokshidhruv@users.noreply.github.com>
2024-11-23 00:09:48 -0500

    Update Footer.jsx

--- ldc-website/src/components/Footer.jsx
+++ ldc-website/src/components/Footer.jsx
@@ -81,7 +81,8 @@ const Footer = () => {
 
                 <a className='font-normal md:text-sm text-xs underline' href='mailto:ldcc@macengsociety.ca'>Become a sponsor!</a>
 
-                <p className="text-xs pt-2">Copyright @ {new Date().getFullYear()} Leadership Development Conference</p>
+                <p className="text-xs pt-4">Copyright @ {new Date().getFullYear()} Leadership Development Conference</p>
+                <p className="text-xs pt-2">Handmade with ❤ using React + Vite & Tailwind</p>
         </footer>
     )
 
